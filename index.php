4918608443afeef45916d133f7a997713f224069
amina <aminaghariani16@gmail.com>
2019-03-07 12:31:58 +0100

    link the blog.css  file

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
+      <link href="<?php bloginfo('template_directory'); ?>/css/blog.css"
+rel="stylesheet">
     <link href="blog.css" rel="stylesheet">
 
 
