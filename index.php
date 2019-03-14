ee161aae71c54e284a71bce4e081840f04809894
amina <aminaghariani16@gmail.com>
2019-03-14 11:57:14 +0100

    division page index

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?> 
+<div class="row">  
+ <div class="col-sm-8 blog-main"> 
+ <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>   <!-- contents of the loop --> <?php endwhile; endif; ?>
+     <?php get_template_part( 'content', get_post_format() ); ?> 
+	   </div> <!-- /.blog-main -->   <?php get_sidebar(); ?>
+	    </div> <!-- /.row --> <?php get_footer(); ?>
