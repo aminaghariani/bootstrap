86cf05133a8ef8c5067ef1fa2362e9e5ad47b768
amina <aminaghariani16@gmail.com>
2019-04-11 19:31:09 +0100

    modification

--- functions.php
+++ functions.php
@@ -1,56 +1,73 @@
 <?php
-// WordPress Titles
-add_theme_support( 'title-tag' );
-// Add scripts and stylesheets
-function startwordpress_scripts() {
- wp_enqueue_style( 'bootstrap', get_template_directory_uri() .
-'/css/bootstrap.min.css', array(), '3.3.6' );
- wp_enqueue_style( 'blog', get_template_directory_uri() . '/css/blog.css'
+function create_post_student(){
+ register_post_type('student',
+ array(
+ 'labels'=>array(
+ 'name'=>__('Students'),
+ ),
+ 'public'=>true,
+ 'hierarchical'=>true,
+ 'has_archive'=>true,
+ 'supports'=>array(
+ 'title',
+'editor',
+'excerpt',
+'thumbnail',
+ ),
+ 'taxonomies'=>array(
+ 'post_tag',
+'category',
+ )
+ )
 );
- wp_enqueue_script( 'bootstrap', get_template_directory_uri() .
-'/js/bootstrap.min.js', array( 'jquery' ), '3.3.6', true );
+ register_taxonomy_for_object_type('category','student');
+ register_taxonomy_for_object_type('post_tag','student');
 }
-add_action( 'wp_enqueue_scripts', 'startwordpress_scripts' );
-// Add Google Fonts
-function startwordpress_google_fonts() {
-wp_register_style('OpenSans',
-'http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800');
-wp_enqueue_style( 'OpenSans');
+add_action('init','create_post_student');
+function add_cv_meta_box(){
+ add_meta_box(
+ 'cv_meta_box',// $id
+ 'cv',// $title
+ 'show_cv_meta_box',// $callback
+ 'student',// $screen
+ 'normal',// $context
+ 'high'// $priority
+);
 }
-add_action('wp_print_styles', 'startwordpress_google_fonts');
-// Custom settings
-function custom_settings_add_menu() {
- add_menu_page( 'Custom Settings', 'Custom Settings', 'manage_options',
-'custom-settings', 'custom_settings_page', null, 99 );
+add_action('add_meta_boxes','add_cv_meta_box');
+function show_cv_meta_box(){
+global $post;
+ $meta=get_post_meta($post->ID , 'cv' , true); ?>
+<input type="hidden" name="your_meta_box_nonce" value="
+ <?php echo wp_create_nonce(basename(__FILE__) );?>">
+<!-- All fields will go here -->
+ <?php
 }
-add_action( 'admin_menu', 'custom_settings_add_menu' );
-// Create Custom Global Settings
-function custom_settings_page() { ?>
-<div class="wrap">
-<h1>Custom Settings</h1>
-<form method="post" action="options.php">
-<?php settings_fields( 'section' );
-do_settings_sections( 'theme-options' );
-submit_button(); ?>
-</form>
-</div>
-<?php }
-// Twitter
-function setting_twitter() { ?>
-<input type="text" name="twitter" id="twitter" value="<?php echo
-get_option( 'twitter' ); ?>" />
-<?php }
-function custom_settings_page_setup() {
-add_settings_section( 'section', 'All Settings', null, 'theme-options' ); 
-add_settings_field( 'twitter', 'Twitter URL', 'setting_twitter', 'themeoptions', 'section' );
-register_setting('section', 'twitter');
-add_settings_field( 'twitter', 'Twitter URL', 'setting_twitter', 'themeoptions', 'section' );
-add_settings_field( 'github', 'GitHub URL', 'setting_github', 'themeoptions', 'section' );
-register_setting( 'section', 'twitter' );
-register_setting( 'section', 'github' );
+function save_cv_meta( $post_id ) {
+// verify nonce
+if ( !wp_verify_nonce( $_POST['your_meta_box_nonce'], basename(__FILE__) ) ) {
+ return $post_id;
 }
-add_action( 'admin_init', 'custom_settings_page_setup' );
-function setting_github() { ?>
-<input type="text" name="github" id="github" value="<?php echo
-get_option('github'); ?>" />
-<?php }
+// check autosave
+if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+ return $post_id;
+}
+// check permissions
+if ( 'page' === $_POST['post_type'] ) {
+ if ( !current_user_can( 'edit_page', $post_id ) ) {
+ return $post_id;
+ } elseif ( !current_user_can( 'edit_post', $post_id ) ) {
+ return $post_id;
+ }
+}
+
+$old = get_post_meta( $post_id, 'cv', true );
+$new = $_POST['cv'];
+if ( $new && $new !== $old ) {
+ update_post_meta( $post_id, 'cv', $new );
+} elseif ( '' === $new && $old ) {
+ delete_post_meta( $post_id, 'cv', $old );
+}
+}
+add_action( 'save_post', 'save_cv_meta' );
+
--- page.php
+++ page.php
@@ -1,9 +1,24 @@
-<?php
+<?php get_header(); ?>
+<div class="row">
+    <div class="col-sm-12">
+        <?php
 $args=array(
-'post_type'=>'your_post',
+'post_type'=>'student',
 );
-$your_loop=new WP_Query($args);
-if($your_loop->have_posts()) : while($your_loop->have_posts()) : $your_loop->the_post();
-$meta=get_post_meta($post->ID , 'your_fields' , true);?>
-<!-- contents of Your Post -->
+$student_loop=new WP_Query($args);
+if($student_loop->have_posts()) : while($student_loop->have_posts()) : $student_loop->the_post();
+$meta=get_post_meta($post->ID , 'student_fields' , true);?>
+<!-- contents of student Post -->
+        <div class="blog-post">
+        <h2 class="blog-post-title">
+        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        
+        </h2>
+        <?php the_excerpt();?>
+        </div>
+    <?php endwhile; endif; wp_reset_postdata();?>
+    </div>
 <?php endwhile; endif; wp_reset_postdata();?>
+    </div> <!-- /.col -->
+</div> <!-- /.row -->
+<?php get_footer(); ?>
